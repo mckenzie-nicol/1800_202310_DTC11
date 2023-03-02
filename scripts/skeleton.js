0782c41ce5ba95c4dc5531bb331db7ffe889f7e6
ALEX DESCHENES <adeschenes@my.bcit.ca>
2023-03-01 17:46:18 -0800

    add map to main.html and skeleton.js

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -4,6 +4,7 @@
 //---------------------------------------------------
 function loadSkeleton(){
     console.log($('#navbarPlaceholder').load('./text/nav.html'));
+    console.log($('#mapPlaceholder').load('./text/map.html'));
     console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
 loadSkeleton();  //invoke the function
