0ab69e3ccbdfdf8f56469e83fc8e82cb29f737eb
ALEX DESCHENES <adeschenes@my.bcit.ca>
2023-03-11 19:46:19 -0800

    add firebase script to all main html pages

--- scripts/map.js
+++ scripts/map.js
@@ -115,13 +115,36 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 // warnings
 function showMapWarnings() {
+    db.collection("reports").doc(reports.uid).get().then(function (doc) {
+        // for report in reports, get lat and lng:
+        
+        
+        // console.log(doc.data().home_lat);
+        // console.log(doc.data().home_lng);
+        warning_lat = doc.data().report_lat
+        warning_lng = doc.data().report_lng
+        var warningIcon = L.icon({
+            // reference html file for icon image
+            iconUrl: 'images/image.jpg',
+            // iconUrl: '.text/map_warning.html',
+            shadowUrl: './images/image.jpg',
 
+            iconSize: [38, 95], // size of the icon
+            shadowSize: [50, 64], // size of the shadow
+            iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+
+        var warning = L.marker([user_home_lat, user_home_lng], { icon: warningIcon }).addTo(map);
+        warning.bindPopup("A warning!").openPopup();
+    });
 }
 var warningIcon = L.icon({
     // reference html file for icon image
-    iconUrl: 'images/drink3.png',
+    iconUrl: 'images/image.jpg',
     // iconUrl: '.text/map_warning.html',
-    shadowUrl: './images/drink1.png',
+    shadowUrl: './images/image.jpg',
 
     iconSize: [38, 95], // size of the icon
     shadowSize: [50, 64], // size of the shadow
@@ -132,7 +155,7 @@ var warningIcon = L.icon({
 var warning = L.marker([51.5, -0.09], { icon: warningIcon }).addTo(map);
 warning.bindPopup("<b>Hello world!</b><br>I am a warning!.").openPopup();
 // home location marker function
-function getHomeCoordinates() {
+function showHomeCoordinates() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             var user = firebase.auth().currentUser;
@@ -140,7 +163,6 @@ function getHomeCoordinates() {
                 // console.log(doc.data().home_lat);
                 // console.log(doc.data().home_lng);
                 user_home_lat = doc.data().home_lat
-                user_home_lng = doc.data().home_lng
                 var homeIcon = L.icon({
                     // reference html file for icon image
                     iconUrl: 'images/drink3.png',
@@ -153,13 +175,16 @@ function getHomeCoordinates() {
                     shadowAnchor: [4, 62],  // the same for the shadow
                     popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
                 });
+                user_home_lng = doc.data().home_lng
+                // map.removeLayer();
+
                 var home = L.marker([user_home_lat, user_home_lng], { icon: homeIcon }).addTo(map);
                 home.bindPopup("Home!").openPopup();
             });
         }
     });
 }
-getHomeCoordinates();
+showHomeCoordinates();
 
 // user pins
 var popup = L.popup();
@@ -187,7 +212,7 @@ function onMapClick(e) {
             home_lat: x,
             home_lng: y,
         })
-        getHomeCoordinates();
+        showHomeCoordinates();
     });
 }
 map.on('click', onMapClick);
