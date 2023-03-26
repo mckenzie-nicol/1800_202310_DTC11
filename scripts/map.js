bef0ba0df1570c92c43a40545ce1163f690c925a
ALEX DESCHENES <adeschenes@my.bcit.ca>
2023-03-26 04:13:23 -0700

    change home icon to use custom html element

--- scripts/map.js
+++ scripts/map.js
@@ -74,9 +74,16 @@ function showHomeCoordinates() {
                 // console.log(doc.data().home_lat);
                 // console.log(doc.data().home_lng);
                 user_home_lat = doc.data().home_lat
-                var homeIcon = L.icon({
+                var homeIcon = L.divIcon({
                     // reference html file for icon image
-                    iconUrl: 'images/drink3.png',
+                    html:
+                        '<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16"> <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />' +
+                        '</svg>' +
+                        '<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class= "bi bi-house-door-fill" viewBox="0 0 16 16" ><path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z" />' +
+                        '</svg >'
+                    ,
+                    className: 'bi bi- exclamation - triangle - fill',
+                    iconUrl: 'text/map_warning.html',
                     // iconUrl: '.text/map_warning.html',
                     shadowUrl: './images/drink1.png',
 
@@ -86,13 +93,13 @@ function showHomeCoordinates() {
                     shadowAnchor: [4, 62],  // the same for the shadow
                     popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
                 });
-                user_home_lng = doc.data().home_lng
-                // map.removeLayer();
+            user_home_lng = doc.data().home_lng
+            // map.removeLayer();
 
-                var home = L.marker([user_home_lat, user_home_lng], { icon: homeIcon }).addTo(map);
-                home.bindPopup("Home!").openPopup();
-            });
-        }
+            var home = L.marker([user_home_lat, user_home_lng], { icon: homeIcon }).addTo(map);
+            home.bindPopup("Home!").openPopup();
+        });
+}
     });
 }
 showHomeCoordinates();
@@ -104,7 +111,7 @@ function onMapClick(e) {
     var user = firebase.auth().currentUser;
     popup
         .setLatLng(e.latlng)
-        .setContent("https://cdn.iconscout.com/icon/free/png-256/pin-locate-marker-location-navigation-16-28668.png"+ "You clicked the map at " + e.latlng.toString())
+        .setContent("https://cdn.iconscout.com/icon/free/png-256/pin-locate-marker-location-navigation-16-28668.png" + "You clicked the map at " + e.latlng.toString())
         .openOn(map);
     console.log(e.latlng);
     console.log(e.latlng["lat"]);
