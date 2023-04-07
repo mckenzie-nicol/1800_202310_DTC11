227dc74303c8a7389879259cfb58531a12b3c440
Brian Lee <clee689@my.bcit.ca>
2023-04-06 18:18:19 -0700

    populate readme

--- scripts/submission.js
+++ scripts/submission.js
@@ -1,3 +1,6 @@
+// this function happens on click on the submitbtn id
+// it reads and writes all the value populated from the modal to firebase including image which is the uploadPic function
+// also alert users for closure upon completion
 $("#submitbtn").click(function () {
     let Text_description = document.getElementById("text_input").value;
     console.log(Text_description)
@@ -35,11 +38,13 @@ $("#submitbtn").click(function () {
 })
 
 var ImageFile;
+// preview the picture uploaded at the frame
 function preview() {
     ImageFile = event.target.files[0]
     frame.src = URL.createObjectURL(ImageFile);
 }
 
+//this function creates a unique ID for the picture uploaded then store it into the cloud storge and return the link to the picture 
 function uploadPic(ReportsDocID) {
     console.log("inside uploadPic " + ReportsDocID);
     var storageRef = storage.ref("images/" + ReportsDocID + ".jpg");
@@ -57,17 +62,20 @@ function uploadPic(ReportsDocID) {
         })
 }
 
+// clear the image at the frame
 function clearImage() {
     document.getElementById('formFile').value = null;
     frame.src = "";
 }
 
+// empty all the fields in the modal
 function empty_all() {
     document.getElementById("text_input").value = "";
     document.getElementById("formFile").value = "";
     clearImage()
 }
 
+// save the userID of the user submitted the report to one of the field inside the report documents
 function saveReportIDforUser(ReportsDocID, UserID) {
     console.log("user id is: " + UserID);
     console.log("postdoc id is: " + ReportsDocID);
